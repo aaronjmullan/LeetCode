e486bb3bf8d2d01f912451632c38d085ceef010e
aaronjmullan <aaron.mullan213@gmail.com>
2023-01-06 23:31:50 +0000

    Create TwoSum Leetcode Challenge Code Solution
    Submitted solution, passed all test cases, C# code above

---
+++ Easy/TwoSum.cs
@@ -0,0 +1,16 @@
+/*
+* LinkedIn - linkedin.com/in/aaronjmullan/
+*/
+
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+
+        for (int i=0; i < nums.Length - 1; i++){
+            for(int j = i + 1; j < nums.Length; j++) {
+                if (nums[i] + nums[j] == target)
+                    return new int[] {i, j};
+            }
+        }        
+        return null;
+    }
+}
